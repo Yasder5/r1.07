3118cf730c37d1e814d3c20620a2e471cbabd596
Yasder5 <102179445+Yasder5@users.noreply.github.com>
2023-10-23 15:45:36 +0200

    Update tp1ex2.py

--- TP1_YB/tp1ex2.py
+++ TP1_YB/tp1ex2.py
@@ -4,4 +4,5 @@ math = 12.5
 anglais = 12.5
 info = 12.5
 promotion = 2023
-print(f"L'eleve {nom} {prenom}
+moy = (math+anglais+info)/3
+print(f"L'étudiant {nom} {prenom} de la promotion {promotion} à une mmoyenne de {moy}")
