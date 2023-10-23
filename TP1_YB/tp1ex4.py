3504fe24986717f54c11bae2db22538cee2961e6
Yasder5 <102179445+Yasder5@users.noreply.github.com>
2023-10-23 15:41:47 +0200

    Update tp1ex4.py

--- TP1_YB/tp1ex4.py
+++ TP1_YB/tp1ex4.py
@@ -1,5 +1,6 @@
 minute = int(input("donner le nombre de minute depuis le debut du mois "))
-heure = minute / 60
-jour = heure /24
+heure = minute // 60
+minute = minute - (heure * 60)
+jour = heure //24
 heure = heure - (jour * 24)
-print(f"il c'est passer {jour} jours et {heure} heures depuis le début du mois")
+print(f"il c'est passer {jour} jours et {heure} heures et {minute} minutes depuis le début du mois")
