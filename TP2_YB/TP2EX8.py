7d09a8781127dba10af7be569935c49f4741ae7e
Yasder5 <102179445+Yasder5@users.noreply.github.com>
2023-11-17 09:55:22 +0100

    Update TP2EX8.py

--- TP2_YB/TP2EX8.py
+++ TP2_YB/TP2EX8.py
@@ -1,5 +1,5 @@
-x = float(input("Entrez un nombre decimal :"))
-if x < 3 and x >= 2 or x > 0 and x <= 1 or x >= -10 and x <= -2:
+x = float(input("Entrez un réel : "))
+if (2 < x < 3 or x == 2) or (0 < x < 1 or x == 1) or (-10 < x < -2 or x == -10 or x == -2):
     print("x appartient à I")
 else:
     print("x n'appartient pas à I")
