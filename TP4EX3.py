dd832294a928d8258dd468ae34c886dc12098f7b
Yasder5 <102179445+Yasder5@users.noreply.github.com>
2023-11-24 11:49:14 +0100

    Add files via upload

---
+++ TP4EX3.py
@@ -0,0 +1,20 @@
+nMax = 10
+v1 = []
+v2 = []
+n = int(input("compris entre 1 et nMax (10) "))
+if n < 1 or n > nMax:
+    while n < 1 or n > nMax:
+        n = int(input("compris entre 1 et nMax (10) "))
+print("Saisie du premier vec")
+for i in range(n):
+    v1.append(int(input("v1")))
+print("Saisie du premier vec")
+for j in range(n):
+    v2.append(int(input("v2")))
+
+
+produitScalaire = 0
+for i in range(n):
+    produitScalaire += v1[i] * v2[i]
+
+print("Le produit sclaire de v1 et v2 vaut ", produitScalaire)
